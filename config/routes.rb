77dc2fac713a446be83235f542cf30a3e089c72f
Mila Zangirolame <milazangirolame@gmail.com>
2017-11-07 15:31:31 -0200

    gerenciador sem ratio buttom ainda

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  # get 'index', to: "tasks#index"
+
+  # get 'show/:id', to: "tasks#show"
+
+  # post 'create', to: "tasks#create"
+
+  # get 'edit', to: "tasks#edit"
+
+  # patch 'update', to: "tasks#update"
+
+  # delete 'delete', to: "tasks#delete"
+
+  root to: "tasks#index"
+  resources :tasks
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
